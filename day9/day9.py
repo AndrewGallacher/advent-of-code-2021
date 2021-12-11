7e6f0560c3a95726cad75b98be877da4d3b55d08
andrew <andrew_gallacher@hotmail.com>
2021-12-11 13:33:31 +0000

    Day 9 part 2

--- day9/day9.py
+++ day9/day9.py
@@ -1,4 +1,7 @@
 # Day 9 
+ 
+from collections import namedtuple
+Point = namedtuple('Point', ['x', 'y'])
 
 def parse_input(filename):
     heightmap = []
@@ -28,45 +31,73 @@ def format_heightmap(heightmap):
     for row in heightmap:
         print("".join(map(lambda i: str(i), row))) 
    
-def is_low_point(heightmap, row, column):
+def is_low_point(heightmap, point):
 
-    height = heightmap[row][column]
+    height = heightmap[point.x][point.y]
 
-    if row > 0:
-        if heightmap[row - 1][column] <= height:
+    for adjacent_point in get_adjacent_points(heightmap, point):
+        if heightmap[adjacent_point.x][adjacent_point.y] <= height:
             return False
-
-    if column > 0:
-        if heightmap[row][column - 1] <= height:
-            return False
-
-    if row < len(heightmap) - 1:
-         if heightmap[row + 1][column] <= height:
-            return False       
-
-    if column < len(heightmap[0]) - 1:
-         if heightmap[row][column + 1] <= height:
-            return False  
 
     return True
 
-def get_basin(heightmap, row, column):
-    return []
+def get_adjacent_points(heightmap, point):
+    points = []
+
+    if point.x > 0:
+        points.append(Point(point.x - 1, point.y))
+        
+    if point.y > 0:
+        points.append(Point(point.x, point.y - 1))
+         
+    if point.x < len(heightmap) - 1:
+        points.append(Point(point.x + 1, point.y))
+         
+    if point.y < len(heightmap[0]) - 1:
+        points.append(Point(point.x, point.y + 1))
+         
+    return points
+
+def get_basin(heightmap, basin, point):
+ 
+    for adjacent_point in get_adjacent_points(heightmap, point):
+        if heightmap[adjacent_point.x][adjacent_point.y] < 9 and not (adjacent_point in basin):
+            basin.append(adjacent_point)
+            basin = get_basin(heightmap, basin, adjacent_point)
+             
+    return basin
 
 # -------------------
 
+#heightmap = parse_input('test.txt')
 heightmap = parse_input('input.txt')
-heightmap = parse_input('test.txt')
 format_heightmap(heightmap)
 
 total_risk = 0
+basin_sizes = []
 
-for i in range(len(heightmap)):
-    for j in range(len(heightmap[0])):
-        if is_low_point(heightmap, i, j):
-            print(i, j, 'is a lowpoint:', heightmap[i][j])
-            total_risk += heightmap[i][j] + 1
+for x in range(len(heightmap)):
+    for y in range(len(heightmap[0])):
+        if is_low_point(heightmap, Point(x, y)):
+            print(x, y, 'is a lowpoint:', heightmap[x][y])
+            total_risk += heightmap[x][y] + 1
+
+            basin = []
+            basin.append(Point(x, y))
+            basin = get_basin(heightmap, basin, Point(x, y))
+            basin_sizes.append(len(basin))
 
 print('total risk:', total_risk)
 
+basin_sizes.sort()
+print('Basin sizes:', basin_sizes)
+ 
+# Find largest 3
+while len(basin_sizes) > 3:
+    basin_sizes.pop(0)
+ 
+product = 1
+for basin_size in basin_sizes:
+    product *= basin_size
 
+print(product)
