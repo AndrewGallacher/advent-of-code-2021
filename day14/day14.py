d623d01e7bff151d230ebd870f1dc45fbc7b47c3
andrew <andrew_gallacher@hotmail.com>
2021-12-15 19:59:45 +0000

    Day 14 - part 2

--- day14/day14.py
+++ day14/day14.py
@@ -1,9 +1,8 @@
 # Day 14 
 
 def read_input(filename):
-    input_lines = []
-    
     input = open(filename, 'r')
+    input_lines = []
       
     for line in input.readlines():    
         input_lines.append(line.replace('\n', '').strip())
@@ -22,13 +21,25 @@ def get_counts(polymer):
 
 # ------------------------------------
 
-filename = 'input.txt'
 filename = 'test.txt'
+filename = 'input.txt'
 input_lines = read_input(filename)
 print(input_lines)
 
+# Parse the polymer template
 polymer_template = input_lines.pop(0)
 print('polymer template: ', polymer_template)
+
+# Count the pairs
+pair_count = {}
+for i in range(len(polymer_template) - 1):
+    pair = polymer_template[i : i + 2]
+    print(pair)
+    if not pair in pair_count:
+        pair_count[pair] = 0
+    pair_count[pair] += 1
+    
+print('Initial pair count:', pair_count)
 
 # Parse the rules
 rules = {}
@@ -36,37 +47,79 @@ for line in input_lines:
     if line.strip() != '':
         data = line.split('->')
         source = data[0].strip()
-        target = source[0] + data[1].strip() + source[1]
-        print(line, source, target)
+        target = data[1].strip()
         rules[source] = target
 
-print(rules)
+print('Rules:', rules)
 
 step = 0
-while step < 10:
+while step < 40:
     step += 1
-    result = 'X'
-    for i in range(len(polymer_template) - 1):
-        result = result[0 : len(result) - 1]
-        pair = polymer_template[i:(i + 2)]
+
+    # Clone the pair count
+    new_pair_count = {}
+    for pair in pair_count:
+        new_pair_count[pair] = pair_count[pair]  
+
+    for pair in pair_count:
         if pair in rules:
-            result += rules[pair]
-        else:
-            result += pair
-    polymer_template = result
-    print('After step', step, len(polymer_template))
+            # Work out the two pairs that this pair will result in
+            new_pair_1 = pair[0] + rules[pair]
+            new_pair_2 = rules[pair] + pair[1]
 
-print(len(polymer_template))
-counts = get_counts(polymer_template)
-print(counts)
- 
+            # Ensure the new pairs are in the collection
+            if not new_pair_1 in new_pair_count:
+                new_pair_count[new_pair_1] = 0
+            if not new_pair_2 in new_pair_count:
+                new_pair_count[new_pair_2] = 0
+                
+            # Update the new counts
+            new_pair_count[pair] -= pair_count[pair]
+            new_pair_count[new_pair_1] += pair_count[pair]
+            new_pair_count[new_pair_2] += pair_count[pair]
+      
+    pair_count = {}
+    for pair in new_pair_count:
+        pair_count[pair] = new_pair_count[pair]
+
+print('Pair counts:', pair_count)
+
+# Since all pairs overlap this will double count
+# Except for the first and last 
+# So set those to one and half the final results
+letter_count = {}
+letter_count[polymer_template[0]] = 1
+letter_count[polymer_template[len(polymer_template) - 1]] = 1
+for pair in pair_count:
+    letter_1 = pair[0]
+    letter_2 = pair[1]
+
+    if not letter_1 in letter_count:
+        letter_count[letter_1] = 0
+    if not letter_2 in letter_count:
+        letter_count[letter_2] = 0
+
+    letter_count[letter_1] += pair_count[pair]
+    letter_count[letter_2] += pair_count[pair]
+        
+# define the most and least common - they will be set to values that exist
 most_common = 0
-least_common = len(polymer_template)
-for key in counts:
-    print(counts[key])
-    if counts[key] > most_common:
-        most_common = counts[key] 
-    if counts[key] < least_common:
-        least_common = counts[key] 
+least_common = 0
+ 
+# Now halve the results
+for letter in letter_count:
+    count = int(letter_count[letter] / 2) 
+    letter_count[letter] = count
+    most_common = count
+    least_common = count
 
-print('Answer:', most_common - least_common)
+print(letter_count)
+  
+for key in letter_count:
+    print(letter_count[key])
+    if letter_count[key] > most_common:
+        most_common = letter_count[key] 
+    if letter_count[key] < least_common:
+        least_common = letter_count[key] 
+
+print('Answer:', most_common - least_common, (most_common - least_common) == 2188189693529)
